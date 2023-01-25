f4b9ed2b5a8f6c234c14dca65e1d11245befde5d
Orlando Caraballo <orlandocaraballo@gmail.com>
2023-01-25 14:52:45 -0500

    Added array.js file

---
+++ 01_25_2023/arrays.js
@@ -0,0 +1,24 @@
+const names = ["orlando", "dylan", "matt", "veronica"];
+
+// initializing an array
+console.log(names[1], names[0], names[3], names[2]);
+console.log(names);
+
+const numbers = [1, 2, 3, 4];
+
+console.log(numbers[2]);
+
+const mixedContentArray = [5, "orlando", false, 6.7];
+
+console.log(mixedContentArray);
+
+// adding content to an array
+names.push("aaron");
+
+console.log(names);
+
+names.unshift("denisse");
+
+console.log(names);
+
+names = [1, 2, 3, 4];
