67962a9608da693c483c2f0d4a8b5388961e882f
Travis Stratton <t.strat7@gmail.com>
2023-02-01 15:55:57 -0600

    Adding repo demo code updates

--- 02_01_2023/event-intro/index.js
+++ 02_01_2023/event-intro/index.js
@@ -1,9 +1,9 @@
 
-
+document.body.style.height = '500vh' // large enough body to scroll
 // Event Handling
 
 let button = document.getElementById('btn')
-
+button.style.position = 'absolute';
 button.addEventListener('click', function(event) {
     console.log("I've been clicked!", event)
     // Anything you want to do goes here
@@ -25,12 +25,38 @@ circle.style.position='absolute';
 document.body.appendChild(circle)
 
 
+window.addEventListener('mousemove', function(event){
+    // console.log(event)
+    circle.style.top = `${event.y-25}px`
+    circle.style.left = `${event.x-25}px`
+    circle.style.zIndex = '-1'
+})
 
-// document.body.addEventListener('mousemove', function(event){
-//     // console.log(event)
-//     circle.style.top = `${event.y-25}px`
-//     circle.style.left = `${event.x-25}px`
-// })
+let r=255, g=125, b=0, rdir=false, gdir=true; bdir=true
+setInterval(() => {
+    r = rdir ? r+5: r-5
+    g = gdir ? g+5: g-5
+    b = bdir ? b+5: b-5
+
+    if (rdir && r >=255) {
+        rdir = false
+    } else if (!rdir && r <= 0) {
+        rdir = true
+    }
+    if (gdir && g >=255) {
+        gdir = false
+    } else if (!gdir && g <= 0) {
+        gdir = true
+    }
+    if (bdir && b >=255) {
+        bdir = false
+    } else if (!bdir && b <= 0) {
+        bdir = true
+    }
+    
+    circle.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+}, 1)
+
 
 
 /*
@@ -49,3 +75,46 @@ Arrow Functions
 - KeyPress Events (Anytime a key is pressed) 
 
 */
+let fontSize = 18
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowUp') {
+        button.style.fontSize = `${fontSize++}px`;
+        console.log('up')
+    }
+})
+
+// rgb(255 255 255)
+let red = 255, green = 255, blue = 255
+document.addEventListener('scroll', function(event) {
+    console.log('scrolling')
+    red--;
+    if (red <= 0) {
+        red = 0
+    }
+    green--;
+    if (green <= 0) {
+        green = 0
+    }
+    blue--;
+    if (blue <= 0) {
+        blue = 0
+    }
+
+    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+})
+
+
+
+document.getElementById('input').addEventListener('change', function(event) {
+    console.log('Typing in the input field.')
+})
+
+
+
+// How you can get styles from elements
+let par = document.querySelector('p')
+let fontSizeFromCSS = window.getComputedStyle(par).fontSize
+let fontSizeNumber = parseInt(fontSizeFromCSS)
+
+console.log(fontSizeFromCSS) // 18px  (it's a string that includes the px)
+console.log(fontSizeNumber) // 18 (parseInt removes the px and converts to number)
